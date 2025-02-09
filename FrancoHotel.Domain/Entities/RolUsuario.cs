62d32705b1f6456e5f8392f7c44fc3ad20562a0d
Eli SsR <suerorodriguezsamuel@gmail.com>
2025-02-09 13:04:41 -0400

    Update

---
+++ FrancoHotel.Domain/Entities/RolUsuario.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using FrancoHotel.Domain.Base;
+
+public sealed class RolUsuario : BaseEntity<int>, IAuditEntity
+{
+    [Key]
+    [Column("IdRolUsuario")]
+    public override int Id { get; set; }
+
+    public string? Descripcion { get; set; }
+    public bool? Estado { get; set; }
+    public DateTime? FechaCreacion { get; set; }
+
+    public RolUsuario() { }
+
+    public RolUsuario(string? descripcion = null)
+    {
+        Descripcion = descripcion;
+        Estado = null;
+        FechaCreacion = null;
+    }
+
+    public RolUsuario(int id, string? descripcion = null, bool? estado = null,
+        DateTime? fechaCreacion = null)
+    {
+        Id = id;
+        Descripcion = descripcion;
+        Estado = estado;
+        FechaCreacion = fechaCreacion;
+    }
+}
