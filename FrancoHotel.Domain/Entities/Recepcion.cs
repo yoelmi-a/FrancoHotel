d0315402e2901837e75da26cdb8e05fc99e881f0
JSEDEKS <josegerardoseverino@gmail.com>
2025-02-10 00:47:05 -0400

    entidades gerardo

---
+++ FrancoHotel.Domain/Entities/Recepcion.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using FrancoHotel.Domain.Base;
+
+namespace FrancoHotel.Domain.Entities
+{
+    internal class Recepcion : BaseEntity<int>
+    {
+        [Column("IdRecepcion")]
+        [Key]
+        public override int Id { get; set; }
+        public int IdCliente {  get; set; }
+        public int IdHabitacion {  get; set; }
+        public DateTime FechaEntrada { get; set; }
+        public DateTime Salida { get; set; }
+        public DateTime FechaSalidaConfirmacion { get; set; }
+        public float PrecioInicial { get; set; }
+        public float Adelanto { get; set; }
+        public float PrecioRestante { get; set; }
+        public float TotalPagado { get; set; }
+        public float CostoPenalidad { get; set; }
+        public string Obsevacion { get; set; }
+        public bool Estado { get; set; }
+    }
+}
---
+++ FrancoHotel.Domain/Entities/Tarifas.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using FrancoHotel.Domain.Base;
+
+namespace FrancoHotel.Domain.Entities
+{
+    internal sealed class Tarifas : BaseEntity<int>
+    {
+        [Key]
+        [Column("IdTarifa")]
+        public override int Id { get; set; }
+        public int IdHabitacion { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public float PrecioPorNoche { get; set; }
+        public float Descuento { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+    }
+}
