9a8cf61ef3bda34dd3b09d6885d009d0ce415393
Yoelmi Alexander Alcala Valdez <20240075@itla.edu.do>
2025-03-02 20:15:09 -0400

    Clase estatica de validacion de datos

---
+++ FrancoHotel.Persistence/Repositories/RepoValidation.cs
@@ -0,0 +1,47 @@
+﻿using FrancoHotel.Domain.Base;
+
+namespace FrancoHotel.Persistence.Repositories
+{
+    public static class RepoValidation
+    {
+        public static OperationResult ValidarString(OperationResult result, string texto)
+        {
+            if (string.IsNullOrWhiteSpace(texto))
+            {
+                result.Success = false;
+            }
+
+            return result;
+        }
+
+        public static OperationResult ValidarEntidad(OperationResult result, Object entidad)
+        {
+            if (entidad == null)
+            {
+                result.Success = false;
+            }
+
+            return result;
+        }
+
+        public static OperationResult ValidarLongitudString(OperationResult result, string texto, int longitud)
+        {
+            if(texto.Length > longitud)
+            {
+                result.Success = false;
+            }
+
+            return result;
+        }
+
+        public static OperationResult ValidarID(OperationResult result, int id)
+        {
+            if(id <= 0)
+            {
+                result.Success = false;
+            }
+
+            return result;
+        }
+    }
+}
